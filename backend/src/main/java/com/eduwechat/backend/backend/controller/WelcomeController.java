b1975f193fa43f5d9862c2f1e82353728ac26c80
Vaskka <1139851358@qq.com>
2019-04-26 13:15:57 +0800

    新增网关欢迎页面，错误处理页面

---
+++ backend/src/main/java/com/eduwechat/backend/backend/controller/WelcomeController.java
@@ -0,0 +1,42 @@
+package com.eduwechat.backend.backend.controller;
+
+
+import com.eduwechat.backend.backend.utils.CommonUtil;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Controller
+public class WelcomeController implements ErrorController {
+
+    @ResponseBody
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return CommonUtil.indexFileContent;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/error", method = RequestMethod.GET)
+    public Map<String, Object> error(HttpServletRequest request) {
+        Map<String, Object> res = new HashMap<>();
+
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+
+        res.put("code", statusCode);
+        res.put("status", "error");
+
+        return res;
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
--- backend/src/main/java/com/eduwechat/backend/backend/utils/CommonUtil.java
+++ backend/src/main/java/com/eduwechat/backend/backend/utils/CommonUtil.java
@@ -2,5 +2,35 @@ package com.eduwechat.backend.backend.utils;
 
 public class CommonUtil {
 
-
+    /**
+     * 首页html
+     */
+    public static final String indexFileContent = "<!DOCTYPE html>\n" +
+            "<html>\n" +
+            "<head>\n" +
+            "\t<title>Welcome</title>\n" +
+            "\t<link href=\"https://staff.bootcss.com/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css\" rel=\"stylesheet\">\n" +
+            "\n" +
+            "\t<style type=\"text/css\">\n" +
+            "\t\t.jumbotron {\n" +
+            "    \t\ttext-align: center;\n" +
+            "\t\t}\n" +
+            "\t</style>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "\t<div class=\"container\">\n" +
+            "\t\t<div class=\"jumbotron\">\n" +
+            "\t\t\t<div class=\"container\">\n" +
+            "\t\t\t\t<h1>New World of High School Education</h1>\n" +
+            "\t\t\t\t<p>This is our WechatApp gateway entry.</p>\n" +
+            "\t\t\t\t<p><a class=\"btn btn-success btn-lg\" href=\"//vaskka.com/mp/swagger-ui.html\" role=\"button\">swagger&nbsp;doc</a></p>\n" +
+            "\t\t\t</div>\n" +
+            "\n" +
+            "\t\t</div>\n" +
+            "\t</div>\n" +
+            "\t<script src=\"https://staff.bootcss.com/npm/jquery@1.12.4/dist/jquery.min.js\"></script>\n" +
+            "\t<script src=\"https://staff.bootcss.com/npm/handlebars@3.0.3/dist/handlebars.min.js\"></script>\n" +
+            "\n" +
+            "</body>\n" +
+            "</html>";
 }
