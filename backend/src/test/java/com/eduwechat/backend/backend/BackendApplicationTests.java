610ffb261aeb17d63ca2977b00629f751fdab949
Vaskka <1139851358@qq.com>
2019-05-22 22:46:58 +0800

    fix test bug

--- backend/src/test/java/com/eduwechat/backend/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/eduwechat/backend/backend/BackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.eduwechat.backend.backend;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
