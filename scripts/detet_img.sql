57354a73da20fea34bdfd8fa184cac0f39a12656
Vaskka <1139851358@qq.com>
2019-05-30 17:02:17 +0800

    删除img的sql

---
+++ scripts/detet_img.sql
@@ -0,0 +1,11 @@
+use edu;
+
+delete from gzsx where content like '%img%';
+delete from gzyw where content like '%img%';
+delete from gzyy where content like '%img%';
+delete from gzwl where content like '%img%';
+delete from gzsw where content like '%img%';
+delete from gzhx where content like '%img%';
+delete from gzdl where content like '%img%';
+delete from gzls where content like '%img%';
+delete from gzzz where content like '%img%';
