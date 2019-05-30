21ae9618ad41342ce6a60d87c868435f50df6a90
Vaskka <1139851358@qq.com>
2019-05-31 04:17:10 +0800

    更新物理和数学的新知识点接口

--- backend/src/main/java/com/eduwechat/backend/backend/controller/common/MathControllerBase.java
+++ backend/src/main/java/com/eduwechat/backend/backend/controller/common/MathControllerBase.java
@@ -2,7 +2,9 @@ package com.eduwechat.backend.backend.controller.common;
 
 import com.eduwechat.backend.backend.controller.base.CanGetKnowledgeTitleListController;
 import com.eduwechat.backend.backend.controller.base.BaseCommonController;
+import com.eduwechat.backend.backend.exceptions.common.ArticleNotFoundException;
 import com.eduwechat.backend.backend.service.common.HighSchoolMathCommonService;
+import com.eduwechat.backend.backend.service.extra.ExtraMathService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Api(description = "数学接口")
@@ -21,6 +24,9 @@ public class MathControllerBase extends BaseCommonController implements CanGetKn
 
     @Autowired
     HighSchoolMathCommonService service;
+
+    @Autowired
+    private ExtraMathService extraService;
 
     @ApiOperation(value = "获取理科、文科数学知识点" ,  notes="指定文理科类型和章节获取数学知识点")
     @ResponseBody
@@ -35,5 +41,55 @@ public class MathControllerBase extends BaseCommonController implements CanGetKn
     @RequestMapping(value = "/knowledge/mapping/get", method = RequestMethod.GET)
     public Map<String, Object> getKnowledgeTitleList() {
         return this.innerGetTitleMappingFromListGetMap(service, "知识点", "knowledge", "sx");
+    }
+
+    @ApiOperation(value = "获取数学新的知识点接口" ,  notes="一起拿全，分页")
+    @ResponseBody
+    @RequestMapping(value = "/new/knowledge/get/{openid}/{number_every_page}/{page_offset}", method = RequestMethod.GET)
+    public Map<String, Object> getNewKnowledge(@PathVariable("openid") String openid,
+                                               @PathVariable(value = "number_every_page") Integer size,
+                                               @PathVariable(value = "page_offset") Integer page) {
+        Map<String, Object> r = new HashMap<>();
+
+        r.put("code", 0);
+        r.put("msg", "success");
+        r.put("data", extraService.getNewKnowledge(size, page));
+
+        return r;
+    }
+
+    @ApiOperation(value = "获取数学新的知识点标题列表" ,  notes="拿到标题list")
+    @ResponseBody
+    @RequestMapping(value = "/new/title/get/{openid}", method = RequestMethod.GET)
+    public Map<String, Object> getNewTitle(@PathVariable("openid") String openid) {
+        Map<String, Object> r = new HashMap<>();
+
+        r.put("code", 0);
+        r.put("msg", "success");
+        r.put("data", extraService.getTotalList());
+
+        return r;
+    }
+
+    @ApiOperation(value = "根据标题筛选数学新的知识点列表" ,  notes="拿到分页数据")
+    @ResponseBody
+    @RequestMapping(value = "/new/title/get/{openid}/{yiji}/{number_every_page}/{page_offset}", method = RequestMethod.GET)
+    public Map<String, Object> getFromYiji(@PathVariable("openid") String openid,
+                                           @PathVariable("yiji") String yiji,
+                                           @PathVariable("number_every_page") Integer size,
+                                           @PathVariable("page_offset") Integer page) {
+        Map<String, Object> r = new HashMap<>();
+
+        try {
+            r.put("data", extraService.getNewKNowledgeFromYiji(yiji, size, page));
+            r.put("code", 0);
+            r.put("msg", "success");
+        } catch (ArticleNotFoundException e) {
+            r.put("data", null);
+            r.put("code", e.getErrorCode());
+            r.put("msg", e.getMessage());
+        }
+
+        return r;
     }
 }
--- backend/src/main/java/com/eduwechat/backend/backend/controller/common/PhysicsControllerBase.java
+++ backend/src/main/java/com/eduwechat/backend/backend/controller/common/PhysicsControllerBase.java
@@ -2,7 +2,9 @@ package com.eduwechat.backend.backend.controller.common;
 
 import com.eduwechat.backend.backend.controller.base.CanGetKnowledgeTitleListController;
 import com.eduwechat.backend.backend.controller.base.BaseCommonController;
+import com.eduwechat.backend.backend.exceptions.common.ArticleNotFoundException;
 import com.eduwechat.backend.backend.service.common.HighSchoolPhysicsCommonService;
+import com.eduwechat.backend.backend.service.extra.ExtraPhysicsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -22,6 +25,9 @@ public class PhysicsControllerBase extends BaseCommonController implements CanGe
 
     @Autowired
     HighSchoolPhysicsCommonService service;
+
+    @Autowired
+    private ExtraPhysicsService extraService;
 
     @ApiOperation(value = "获取物理知识点" ,  notes="指定必修选修知识点")
     @ResponseBody
@@ -36,5 +42,55 @@ public class PhysicsControllerBase extends BaseCommonController implements CanGe
     @RequestMapping(value = "/knowledge/mapping/get", method = RequestMethod.GET)
     public Map<String, Object> getKnowledgeTitleList() {
         return this.innerGetTitleMappingFromListGetMap(service, "知识点", "knowledge", "wl");
+    }
+
+    @ApiOperation(value = "获取物理新的知识点接口" ,  notes="一起拿全，分页")
+    @ResponseBody
+    @RequestMapping(value = "/new/knowledge/get/{openid}/{number_every_page}/{page_offset}", method = RequestMethod.GET)
+    public Map<String, Object> getNewKnowledge(@PathVariable("openid") String openid,
+                                               @PathVariable(value = "number_every_page") Integer size,
+                                               @PathVariable(value = "page_offset") Integer page) {
+        Map<String, Object> r = new HashMap<>();
+
+        r.put("code", 0);
+        r.put("msg", "success");
+        r.put("data", extraService.getNewKnowledge(size, page));
+
+        return r;
+    }
+
+    @ApiOperation(value = "获取物理新的知识点标题列表" ,  notes="拿到标题list")
+    @ResponseBody
+    @RequestMapping(value = "/new/title/get/{openid}", method = RequestMethod.GET)
+    public Map<String, Object> getNewTitle(@PathVariable("openid") String openid) {
+        Map<String, Object> r = new HashMap<>();
+
+        r.put("code", 0);
+        r.put("msg", "success");
+        r.put("data", extraService.getTotalList());
+
+        return r;
+    }
+
+    @ApiOperation(value = "根据标题筛选物理新的知识点列表" ,  notes="拿到分页数据")
+    @ResponseBody
+    @RequestMapping(value = "/new/title/get/{openid}/{yiji}/{number_every_page}/{page_offset}", method = RequestMethod.GET)
+    public Map<String, Object> getFromYiji(@PathVariable("openid") String openid,
+                                           @PathVariable("yiji") String yiji,
+                                           @PathVariable("number_every_page") Integer size,
+                                           @PathVariable("page_offset") Integer page) {
+        Map<String, Object> r = new HashMap<>();
+
+        try {
+            r.put("data", extraService.getNewKNowledgeFromYiji(yiji, size, page));
+            r.put("code", 0);
+            r.put("msg", "success");
+        } catch (ArticleNotFoundException e) {
+            r.put("data", null);
+            r.put("code", e.getErrorCode());
+            r.put("msg", e.getMessage());
+        }
+
+        return r;
     }
 }
