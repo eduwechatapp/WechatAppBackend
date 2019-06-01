e1d7c9ce3d41f45574908d1b7a99d2610f830424
Vaskka <1139851358@qq.com>
2019-06-02 03:28:08 +0800

    修改换行问题

--- scripts/deal_new/remove_unuse_text.py
+++ scripts/deal_new/remove_unuse_text.py
@@ -19,43 +19,46 @@ res_g = col_g.find({"title": {"$eq": "中国的河流和湖泊"}})
 
 for r in res_g:
 
-    parser = etree.HTML(r["content"])
+    # parser = etree.HTML(r["content"])
 
-    bodys = parser.xpath("//body")
+    # bodys = parser.xpath("//body")
 
-    for body in bodys:
-        attrs = body.attrib
-        if "style" in attrs:
-            attrs["style"] = attrs["style"] + "overflow: auto;"
-        else:
-            attrs["style"] = "overflow: auto;"
+    # for body in bodys:
+    #     attrs = body.attrib
+    #     if "style" in attrs:
+    #         attrs["style"] = attrs["style"] + "overflow: auto;"
+    #     else:
+    #         attrs["style"] = "overflow: auto;"
 
-    spans = parser.xpath("//span")
+    # spans = parser.xpath("//span")
 
-    for span in spans:
-        attrs = span.attrib
-        if "style" in attrs:
-            attrs["style"] = attrs["style"] + ";display: inline;margin: 0;padding: 0;overflow: auto;"
-        else:
-            attrs["style"] = ";display: inline;margin: 0;padding: 0;overflow: auto;"
+    # for span in spans:
+    #     attrs = span.attrib
+    #     if "style" in attrs:
+    #         attrs["style"] = attrs["style"] + ";display: inline;margin: 0;padding: 0;overflow: auto;"
+    #     else:
+    #         attrs["style"] = ";display: inline;margin: 0;padding: 0;overflow: auto;"
 
-    tds = parser.xpath("//td")
+    # tds = parser.xpath("//td")
 
-    for td in tds:
-        attrs = td.attrib
-        attrs["style"] = attrs["style"] + ";flex:1;padding:5px;font-size:28rpx;border-left:1px solid #e0e0e0;word-break: break-all;overflow: auto;"
+    # for td in tds:
+    #     attrs = td.attrib
+    #     attrs["style"] = attrs["style"] + ";flex:1;padding:5px;font-size:28rpx;border-left:1px solid #e0e0e0;word-break: break-all;overflow: auto;"
 
-    trs = parser.xpath("//tr")
+    # trs = parser.xpath("//tr")
 
-    for tr in trs:
-        attrs = tr.attrib
-        attrs["style"] = attrs["style"] + ";display: flex;border-right:1px solid #e0e0e0;border-bottom:1px solid #e0e0e0;border-top:1px solid #e0e0e0;overflow: auto;"
+    # for tr in trs:
+    #     attrs = tr.attrib
+    #     attrs["style"] = attrs["style"] + ";display: flex;border-right:1px solid #e0e0e0;border-bottom:1px solid #e0e0e0;border-top:1px solid #e0e0e0;overflow: auto;"
 
     _id = r["_id"]
 
-    after = etree.tostring(parser, encoding="utf-8").decode("utf-8").replace("&#10;", '').replace('\n', '').replace('\r', '').replace('\t', '')
-
-    col_g.update_one({"_id": _id}, {"$set": {'content': after}})
+    # after = etree.tostring(parser, encoding="utf-8").decode("utf-8").replace("&#10;", '').replace('\n', '').replace('\r', '').replace('\t', '').replace("\\n, ''")
+    # print(after)
+    # with open("a.html", "w", encoding="utf-8") as f:
+    #     f.write(after)
+    # print(after)
+    col_g.update_one({"_id": _id}, {"$set": {'content': r["content"].replace("\\n", '')}})
 
 # for r in res_p:
 
